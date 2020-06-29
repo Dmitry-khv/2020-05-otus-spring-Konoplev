2e22c612c876da2f58bbb394c2e4e35076e8702e
konoplevdmitry <kono>
2020-06-29 23:14:23 +1000

    remove messageSource

--- hw-02/src/main/java/ru/otus/hw2/config/AppConfig.java
+++ hw-02/src/main/java/ru/otus/hw2/config/AppConfig.java
@@ -1,22 +1,12 @@
 package ru.otus.hw2.config;
 
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import ru.otus.hw2.service.IOService;
 import ru.otus.hw2.service.IOServiceImpl;
 
 @Configuration
 public class AppConfig {
-
-    @Bean
-    public MessageSource messageSource() {
-        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename("/messages");
-        messageSource.setDefaultEncoding("UTF-8");
-        return messageSource;
-    }
 
     @Bean
     public IOService ioService() {
