e80023ec1a214ec1e08f1eb8273e6d2c4a996eab
konoplevdmitry <kono>
2020-09-24 13:55:44 +1000

    added simple test

--- hw-10/src/test/java/ru/otus/library/rest/BookControllerTest.java
+++ hw-10/src/test/java/ru/otus/library/rest/BookControllerTest.java
@@ -1,12 +1,17 @@
 package ru.otus.library.rest;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
+import ru.otus.library.domain.Book;
+import ru.otus.library.repository.BookRepository;
 
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,10 +20,25 @@ class BookControllerTest {
 
     @Autowired
     private TestRestTemplate testRestTemplate;
+    @Autowired
+    private BookRepository bookRepository;
+
+    private ResponseEntity responseEntity;
+    private static final String BOOK_ID = "1";
 
     @Test
     public void shouldGetAllBooks() {
-        ResponseEntity responseEntity = testRestTemplate.getForEntity("/api/books", Object.class);
+        responseEntity = testRestTemplate.getForEntity("/api/book/list", Object.class);
         assertThat(responseEntity.getStatusCodeValue()).isBetween(200, 400);
+    }
+
+    @Test
+    public void shouldGetBookById() {
+        responseEntity = testRestTemplate.getForEntity(String.format("/api/book/%s/view", BOOK_ID), Object.class);
+        assertThat(responseEntity.getStatusCodeValue()).isBetween(200, 400);
+        Object obj = responseEntity.getBody();
+        Book bookFromWeb = new ObjectMapper().convertValue(obj, Book.class);
+        Book bookFromDb = bookRepository.findById(BOOK_ID).get();
+        assertThat(bookFromWeb).isEqualTo(bookFromDb);
     }
 }
